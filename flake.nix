fb74cd893d4ab84fb596fd6890041c7c0502972a
vpletea <84437690+vpletea@users.noreply.github.com>
2025-09-10 14:40:15 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -16,5 +16,13 @@
       ./hp/host.nix
       ];
     };
+    nixosConfigurations."f2" = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
+    modules = [
+      ./f2/machine.nix
+      ./f2/services.nix
+      ];
+    };
   };
 }
